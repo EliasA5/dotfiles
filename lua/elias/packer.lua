56872cbda5fd846bd216adba171dc84ff8246a0e
Elias Assaf <elyas51000@gmail.com>
2023-02-19 01:22:11 +0200

    add lsp

--- lua/elias/packer.lua
+++ lua/elias/packer.lua
@@ -23,6 +23,28 @@ return require('packer').startup(function(use)
   use 'christoomey/vim-tmux-navigator'
   use 'szw/vim-maximizer'
   use 'tpope/vim-commentary'
+  use {
+	  'VonHeikemen/lsp-zero.nvim',
+	  branch = 'v1.x',
+	  requires = {
+		  -- LSP Support
+		  {'neovim/nvim-lspconfig'},             -- Required
+		  {'williamboman/mason.nvim'},           -- Optional
+		  {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+		  -- Autocompletion
+		  {'hrsh7th/nvim-cmp'},         -- Required
+		  {'hrsh7th/cmp-nvim-lsp'},     -- Required
+		  {'hrsh7th/cmp-buffer'},       -- Optional
+		  {'hrsh7th/cmp-path'},         -- Optional
+		  {'saadparwaiz1/cmp_luasnip'}, -- Optional
+		  {'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+		  -- Snippets
+		  {'L3MON4D3/LuaSnip'},             -- Required
+		  {'rafamadriz/friendly-snippets'}, -- Optional
+	  }
+  }    
 
 end)
 
