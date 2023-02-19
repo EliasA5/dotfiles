5aca34f01b74d3e397192236f2061a758ed112d8
Elias Assaf <elyas51000@gmail.com>
2023-02-19 14:55:20 +0200

    more remaps

--- lua/elias/remap.lua
+++ lua/elias/remap.lua
@@ -7,14 +7,20 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
 
-vim.keymap.set("n", "<leader>l/", ":noh<CR>")
+vim.keymap.set("x", "<leader>p", "\"_dP")
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
 
 
+vim.keymap.set("n", "<leader>s", [[:.,$s/\<<C-r><C-w>\>//gcI<Left><Left><Left><Left>]])
+--vim.keymap.set("n", "<leader>x", "<cmd>!chmod u+x %<CR>", { silent = true })
 
+-- vim.keymap.set("n", "<leader><leader>", function() vim.cmd("so") end)
 
-
-
+vim.keymap.set("n", "<leader>l/", ":noh<CR>", {silent = true})
 
 
 
