3c477a3b27e49a7d4e9b07c2d356896611e7384e
Elias Assaf <elyas51000@gmail.com>
2023-02-25 17:36:20 +0200

    change whichkey options

--- lua/elias/packer.lua
+++ lua/elias/packer.lua
@@ -53,18 +53,7 @@ return require('packer').startup(function(use)
   use 'nvim-tree/nvim-web-devicons'
   use 'freddiehaddad/feline.nvim'
   use 'nvim-tree/nvim-tree.lua'
-  use {
-      "folke/which-key.nvim",
-      config = function()
-          vim.o.timeout = true
-          vim.o.timeoutlen = 300
-          require("which-key").setup {
-              -- your configuration comes here
-              -- or leave it empty to use the default settings
-              -- refer to the configuration section below
-          }
-      end
-  }
+  use 'folke/which-key.nvim' 
 
 end)
 
