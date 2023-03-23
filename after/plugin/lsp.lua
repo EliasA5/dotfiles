e0b6a575689354773c15ad3ea8316c88ec5bcfcb
Elias Assaf <elyas51000@gmail.com>
2023-03-23 20:13:21 +0200

    add jump to declaration/definition and ensure erlang parser is installed

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,10 +1,12 @@
 local lsp = require('lsp-zero').preset({
-  name = 'minimal',
+  name = 'recommended',
   set_lsp_keymaps = true,
   manage_nvim_cmp = true,
   suggest_lsp_servers = false,
 })
 
+vim.keymap.set('n', 'gD', vim.lsp.buf.declaration)
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
 -- (Optional) Configure lua language server for neovim
 lsp.nvim_workspace()
 
