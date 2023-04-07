4f00dda2411413029f52579264e188cb8b905331
Elias Assaf <elyas51000@gmail.com>
2023-04-07 20:15:54 +0300

    more usefull mappings

--- lua/elias/remap.lua
+++ lua/elias/remap.lua
@@ -3,6 +3,7 @@ vim.keymap.set("n", "<leader>fs", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("n", "<leader>a", ":keepjumps normal! ggVG<cr>")
 
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
@@ -11,9 +12,8 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 vim.keymap.set("x", "<leader>p", "\"_dP")
-vim.keymap.set("n", "<leader>d", "\"_d")
-vim.keymap.set("v", "<leader>d", "\"_d")
-
+vim.keymap.set({"n", "v"}, "<leader>d", "\"_d")
+vim.keymap.set({"n", "x"}, "x", "\"_x")
 
 vim.keymap.set("n", "<leader>ss", [[:.,$s/\<<C-r><C-w>\>//gcI<Left><Left><Left><Left>]])
 --vim.keymap.set("n", "<leader>x", "<cmd>!chmod u+x %<CR>", { silent = true })
