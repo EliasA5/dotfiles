86f13a45c4f7ecff5fc2ca81c9c55189907829be
Elias Assaf <elyas51000@gmail.com>
2023-04-16 14:48:44 +0300

    use mini.comment instead of commentary

--- lua/elias/packer.lua
+++ lua/elias/packer.lua
@@ -8,7 +8,7 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   use 'Mofiqul/dracula.nvim'
-  use 'tpope/vim-commentary'
+  use 'echasnovski/mini.comment'
   use { 'echasnovski/mini.surround',  config = function() require('mini.surround').setup() end }
 
 end)
