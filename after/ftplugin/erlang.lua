15d0196bc76dc8957f81dc1b85e1a778261c7b92
Elias Assaf <elyas51000@gmail.com>
2023-06-14 23:04:34 +0300

    update tab settings on erlang files

--- after/ftplugin/erlang.lua
+++ after/ftplugin/erlang.lua
@@ -1,3 +1,11 @@
 
-vim.opt_local.commentstring = '%% %s'
+local set = vim.opt_local
+
+set.commentstring = '%% %s'
+
+set.tabstop = 2
+set.softtabstop = 0
+set.shiftwidth = 2
+set.expandtab = true
+set.smarttab = true
 
