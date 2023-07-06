080e94f9d7580d49548ea21e082be3038a1939f7
Elias Assaf <elyas51000@gmail.com>
2023-07-06 22:03:40 +0300

    remove iron

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -35,7 +35,6 @@ return
     }
 	},
     'voldikss/vim-floaterm',
-    {'hkupty/iron.nvim'},
     {
         'lewis6991/gitsigns.nvim',
         -- tag = 'release' -- To use the latest release (do not use this if you run Neovim nightly or dev builds!)
