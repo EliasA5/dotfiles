e30d4237fea628b18c6571045842960827ed4a9f
Elias Assaf <elyas51000@gmail.com>
2023-07-06 22:36:41 +0300

    add leap.nvim for faster navigation (s keybind)

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -55,4 +55,5 @@ return
 	},
 	{'EliasA5/pastem', config = function() require('pastem').setup() end},
 	{'EliasA5/erlang-skeletons', config = function() require('erlang-skeletons').setup() end},
+	{'ggandor/leap.nvim', lazy = false, config = function() require('leap').add_default_mappings() end, },
 }
