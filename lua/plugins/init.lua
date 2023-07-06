c66df722c6d5ea0e2e5806537ab928f4e9433892
Elias Assaf <elyas51000@gmail.com>
2023-07-06 23:18:39 +0300

    relocate config of mini surround to another folder

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,6 +1,6 @@
 return
 {
 	{ 'echasnovski/mini.comment', config = function() require('mini.comment').setup() end },
-	{ 'echasnovski/mini.surround',  config = function() require('mini.surround').setup() end },
-	{ 'ggandor/leap.nvim', config = function() require('leap').add_default_mappings() end },
+	{ 'echasnovski/mini.surround'},
+	{ 'ggandor/leap.nvim', lazy = false, config = function() require('leap').add_default_mappings() end },
 }
