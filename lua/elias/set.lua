d6440d37b3cbc0a284cab8d31e5689a1974704df
Elias Assaf <elyas51000@gmail.com>
2023-11-27 20:41:48 +0200

    fix windows clipboard

--- lua/elias/set.lua
+++ lua/elias/set.lua
@@ -1,5 +1,20 @@
 local set = vim.opt
-set.clipboard = "unnamedplus"
+-- set.clipboard = "unnamedplus"
+vim.api.nvim_exec(
+[[
+          let g:clipboard = {
+          \   'name': 'win32yank-wsl',
+          \   'copy': {
+          \      '+': 'win32yank.exe -i --crlf',
+          \      '*': 'win32yank.exe -i --crlf',
+          \    },
+          \   'paste': {
+          \      '+': 'win32yank.exe -o --lf',
+          \      '*': 'win32yank.exe -o --lf',
+          \   },
+          \   'cache_enabled': 0,
+          \ }
+                ]], true)
 
 set.guicursor = ""
 
