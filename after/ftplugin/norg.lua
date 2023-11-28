36f48fb4cae259230dbcde66f98c280637b4d843
Elias Assaf <elyas51000@gmail.com>
2023-11-28 12:54:05 +0200

    configure norg filetypes

--- after/ftplugin/norg.lua
+++ after/ftplugin/norg.lua
@@ -1,3 +1,12 @@
 
 vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, { pattern = {"*.norg"}, command = "set conceallevel=3" })
 
+
+local set = vim.opt_local
+
+set.tabstop = 2
+set.softtabstop = 0
+set.shiftwidth = 2
+set.expandtab = true
+set.smarttab = true
+
