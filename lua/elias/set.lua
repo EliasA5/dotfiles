706919486d47496cdf25062507edd7b67e879544
Elias Assaf <elyas51000@gmail.com>
2023-12-05 09:39:08 +0200

    fix clipboard on wsl

--- lua/elias/set.lua
+++ lua/elias/set.lua
@@ -1,20 +1,20 @@
 local set = vim.opt
--- set.clipboard = "unnamedplus"
+set.clipboard = "unnamedplus"
 vim.api.nvim_exec(
 [[
-          let g:clipboard = {
-          \   'name': 'win32yank-wsl',
-          \   'copy': {
-          \      '+': 'win32yank.exe -i --crlf',
-          \      '*': 'win32yank.exe -i --crlf',
-          \    },
-          \   'paste': {
-          \      '+': 'win32yank.exe -o --lf',
-          \      '*': 'win32yank.exe -o --lf',
-          \   },
-          \   'cache_enabled': 0,
-          \ }
-                ]], true)
+             let g:clipboard = {
+         \   'name': 'WslClipboard',
+         \   'copy': {
+         \      '+': 'clip.exe',
+         \      '*': 'clip.exe',
+         \    },
+         \   'paste': {
+         \      '+': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+         \      '*': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+         \   },
+         \   'cache_enabled': 0,
+         \ }  
+  ]], true)
 
 set.guicursor = ""
 
