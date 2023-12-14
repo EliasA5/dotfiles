1503eb9f837172e24c101de7c08d7cce69eb7531
Elias Assaf <elyas51000@gmail.com>
2023-12-14 14:21:35 +0200

    add keybinds to open Neorg

--- after/plugin/neorg.lua
+++ after/plugin/neorg.lua
@@ -12,3 +12,7 @@ require("neorg").setup {
           },
         },
       }
+
+vim.keymap.set("n", "<leader>ni", ":Neorg index<CR>", {desc = "Open Neorg workspace index", silent = true, })
+vim.keymap.set("n", "<leader>nr", ":Neorg return<CR>", {desc = "Return from Neorg", silent = true, })
+
