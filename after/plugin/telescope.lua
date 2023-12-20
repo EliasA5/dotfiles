63c5158189547a107d6385a6784ec7e808c666ca
Elias Assaf <elyas51000@gmail.com>
2023-12-20 16:29:19 +0200

    add binding to search for selected visual text

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -25,6 +25,15 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = "Fuzzy find files"
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {desc = "Live grep inside files"})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {desc = "Search in files indexed by git"})
 vim.keymap.set('n', '<leader>fw', builtin.grep_string, {desc = "Search for word under cursor"})
+vim.keymap.set('v', '<leader>fw', function()
+  -- TODO wait for a better way to get visual selection value
+  vim.cmd('noau normal! "vy"')
+	local text = vim.fn.getreg('v')
+	vim.fn.setreg('v', {})
+
+	text = string.gsub(text, "\n", "")
+  builtin.grep_string({ search = text,})
+end, {desc = "Search for visual selection"})
 vim.keymap.set('n', 'gd', builtin.lsp_definitions, {desc = "LSP Defnition"})
 vim.keymap.set('n', 'gD', builtin.lsp_type_definitions, {desc = "LSP Type Definition"})
 vim.keymap.set('n', 'gr', builtin.lsp_references, {desc = "LSP References"})
