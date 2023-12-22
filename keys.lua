63322f174916abc01c7f8f4704108e47e780615b
Elias Assaf <elyas51000@gmail.com>
2023-12-22 20:14:09 +0200

    add keybind to toggle passthrough to embedded terminal (tmux)

--- keys.lua
+++ keys.lua
@@ -42,11 +42,17 @@ local function is_inside_tmux(pane)
   if name == nil then return false end
   local prog = basename(name)
   -- wez:log_info(prog == 'tmux')
-  wez.log_info('inside tmux: ' .. prog)
+  -- wez.log_info('inside tmux: ' .. prog)
   return prog == 'tmux'
 end
 
 local function is_outside_tmux(pane) return not is_inside_tmux(pane) end
+
+passthrough = true
+
+local function is_passthrough(pane) return passthrough end
+local function isnt_passthrough(pane) return not passthrough end
+local function toggle_passthrough() passthrough = not passthrough end
 
 local function and_2_functions(f1, f2)
   return (function(pane)
@@ -56,19 +62,12 @@ local function and_2_functions(f1, f2)
 end
 
 return {
-  {
-    key = 'v',
-    mods = 'LEADER',
-    action = act.SplitPane {
-      direction = 'Right',
-      size = { Percent = 40 },
-    },
-  },
-   bind_if(is_outside_tmux, 'h', 'LEADER', act.SplitVertical({domain = 'CurrentPaneDomain' })),
-   bind_if(is_outside_tmux, 'x', 'LEADER', act.CloseCurrentPane({confirm = true })),
-   bind_if(is_outside_tmux, 'c', 'LEADER', act.SpawnTab('CurrentPaneDomain')),
-   bind_if(is_outside_tmux, 'm', 'LEADER', act.TogglePaneZoomState),
-   bind_if(is_outside_tmux, 'd', 'LEADER', act.Nop),
+   bind_if(isnt_passthrough, 'v', 'LEADER', act.SplitPane ({direction = 'Right', size = { Percent = 40 }, })),
+   bind_if(isnt_passthrough, 'h', 'LEADER', act.SplitVertical({domain = 'CurrentPaneDomain' })),
+   bind_if(isnt_passthrough, 'x', 'LEADER', act.CloseCurrentPane({confirm = true })),
+   bind_if(isnt_passthrough, 'c', 'LEADER', act.SpawnTab('CurrentPaneDomain')),
+   bind_if(isnt_passthrough, 'm', 'LEADER', act.TogglePaneZoomState),
+   bind_if(isnt_passthrough, 'd', 'LEADER', act.Nop),
   {
     key = 'Space',
     mods = 'LEADER',
@@ -79,37 +78,29 @@ return {
     mods = 'LEADER',
     action = act.ReloadConfiguration,
   },
-   bind_if(is_outside_tmux, 'LeftArrow', 'LEADER', act.AdjustPaneSize({'Left', 5})),
-   bind_if(is_outside_tmux, 'DownArrow', 'LEADER', act.AdjustPaneSize({'Down', 5})),
-   bind_if(is_outside_tmux, 'UpArrow', 'LEADER', act.AdjustPaneSize({'Up', 5})),
-   bind_if(is_outside_tmux, 'RightArrow', 'LEADER', act.AdjustPaneSize({'Right', 5})),
-  {
-    key = 'RightArrow',
-    mods = 'CTRL|SHIFT',
-    action = act.ActivateTabRelative(1)
-  },
-  {
-    key = 'LeftArrow',
-    mods = 'CTRL|SHIFT',
-    action = act.ActivateTabRelative(-1)
-  },
-  {
-    key = 'l',
-    mods = 'CTRL|SHIFT',
-    action = act.ActivateTabRelative(1)
-  },
-  {
-    key = 'h',
-    mods = 'CTRL|SHIFT',
-    action = act.ActivateTabRelative(-1)
-  },
+   bind_if(isnt_passthrough, 'LeftArrow', 'LEADER', act.AdjustPaneSize({'Left', 5})),
+   bind_if(isnt_passthrough, 'DownArrow', 'LEADER', act.AdjustPaneSize({'Down', 5})),
+   bind_if(isnt_passthrough, 'UpArrow', 'LEADER', act.AdjustPaneSize({'Up', 5})),
+   bind_if(isnt_passthrough, 'RightArrow', 'LEADER', act.AdjustPaneSize({'Right', 5})),
+   bind_if(isnt_passthrough, 'RightArrow', 'CTRL', act.ActivateTabRelative(1)),
+   bind_if(isnt_passthrough, 'LeftArrow', 'CTRL', act.ActivateTabRelative(-1)),
+   -- bind_if(isnt_passthrough, 'L', 'CTRL|SHIFT', act.ActivateTabRelative(1)),
+   -- bind_if(isnt_passthrough, 'H', 'CTRL|SHIFT', act.ActivateTabRelative(-1)),
   {
     key = 'l',
     mods = 'LEADER',
     action = act.ShowLauncher,
   },
-   bind_if(and_2_functions(is_outside_tmux, is_outside_vim), 'h', 'CTRL', act.ActivatePaneDirection('Left')),
-   bind_if(and_2_functions(is_outside_tmux, is_outside_vim), 'l', 'CTRL', act.ActivatePaneDirection('Right')),
-   bind_if(and_2_functions(is_outside_tmux, is_outside_vim), 'j', 'CTRL', act.ActivatePaneDirection('Down')),
-   bind_if(and_2_functions(is_outside_tmux, is_outside_vim), 'k', 'CTRL', act.ActivatePaneDirection('Up')),
+   bind_if(isnt_passthrough, 'h', 'CTRL', act.ActivatePaneDirection('Left')),
+   bind_if(isnt_passthrough, 'l', 'CTRL', act.ActivatePaneDirection('Right')),
+   bind_if(isnt_passthrough, 'j', 'CTRL', act.ActivatePaneDirection('Down')),
+   bind_if(isnt_passthrough, 'k', 'CTRL', act.ActivatePaneDirection('Up')),
+  {
+    key = 'p',
+    mods = 'LEADER',
+    action = wez.action_callback(function(_,_)
+      toggle_passthrough()
+      -- wezterm.log_info(passthrough)
+    end),
+  },
 }
