00c6aa370ffab5372c94c6bab5b314fc19a535b1
Elias Assaf <eassaf@localhost.localdomain>
2024-03-18 10:46:14 +0200

    arrow write if file was added or removed

--- after/plugin/arrow.lua
+++ after/plugin/arrow.lua
@@ -14,5 +14,14 @@ require('arrow').setup({
   },
   separate_by_branch = true,
 })
-vim.keymap.set("n", "<leader>ha", require("arrow.persist").toggle)
+
+vim.keymap.set("n", "<leader>ha", function()
+  require("arrow.persist").toggle()
+  local ans = "Removed file from arrow list"
+  local is_arrow = require('arrow.statusline').is_on_arrow_file()
+  if is_arrow then
+    ans = "Added file to arrow list"
+  end
+  print(ans)
+end,{ desc = "Add or remove file from arrow list"} )
 
