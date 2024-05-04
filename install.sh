6d037240558883d05bddd08e43cce2b5c53ded66
Elias Assaf <elyas51000@gmail.com>
2024-05-04 11:23:45 +0300

    worktree add origin branch, add lazygit config
    Signed-off-by: Elias Assaf <elyas51000@gmail.com>

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then exit 0; fi
-declare -a all=("nvim" "tmux" "wezterm" "nvim-code" "kitty" "wezterm-windows" "nvim-wsl")
+declare -a all=("nvim" "tmux" "wezterm" "nvim-code" "kitty" "wezterm-windows" "nvim-wsl" "lazygit")
 
-declare -a for_linux=("nvim" "tmux" "wezterm" "nvim-code" "kitty")
+declare -a for_linux=("nvim" "tmux" "wezterm" "nvim-code" "kitty" "lazygit")
 
 if [[ ${all[@]} =~ $1 ]]; then
-	git worktree add ~/.config/$1 $1
+	git worktree add ~/.config/$1 origin/$1
 	exit 0
 else
 	echo "invalid name, valid configs: (${all[@]})"
@@ -16,7 +16,7 @@ fi
 if [[ "$1" == "linux" ]]; then
 	for i in "${for_linux[@]}"
 	do
-		git worktree add ~/.config/$i $i
+		git worktree add ~/.config/$i origin/$i
 	done
 	exit 0
 fi
@@ -26,7 +26,7 @@ declare -a for_windows=("wezterm-windows")
 if [[ "$1" == "windows" ]]; then
 	for i in "${for_windows[@]}"
 	do
-		git worktree add ../$i $i
+		git worktree add ../$i origin/$i
 	done
 	exit 0
 fi
@@ -36,7 +36,7 @@ declare -a for_wsl=("nvim-wsl" "tmux" "nvim-code")
 if [[ "$1" == "wsl" ]]; then
 	for i in "${for_wsl[@]}"
 	do
-		git worktree add ~/.config/$i $i
+		git worktree add ~/.config/$i origin/$i
 	done
 	exit 0
 fi
