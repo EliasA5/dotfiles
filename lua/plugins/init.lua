aac3b5542cf684b0fb3dff2c536ba53f90fd793e
Elias Assaf <elyas51000@gmail.com>
2024-06-10 01:17:00 +0300

    move quickfix reflector to lazy
    Signed-off-by: Elias Assaf <elyas51000@gmail.com>

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,7 +5,6 @@ return
     keys = {{"<leader>m", vim.cmd.MaximizerToggle, desc = "toggle maximization"}}
   },
   {'EliasA5/pastem', config = function() require('pastem').setup() end},
-  'stefandtw/quickfix-reflector.vim',
   -- disable on nvim 0.10.*
   -- enable on nvim 0.10.*
   -- {
