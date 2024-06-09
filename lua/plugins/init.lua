15766c408b6456098058f41b7b7a1b6ad196e145
Elias Assaf <elyas51000@gmail.com>
2024-06-10 01:09:56 +0300

    move lsp to lazy
    Signed-off-by: Elias Assaf <elyas51000@gmail.com>

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,18 +4,6 @@ return
     'szw/vim-maximizer',
     keys = {{"<leader>m", vim.cmd.MaximizerToggle, desc = "toggle maximization"}}
   },
-  {
-    'williamboman/mason.nvim',
-    build = function()
-      pcall(vim.cmd, 'MasonUpdate')
-    end,
-  },
-  'williamboman/mason-lspconfig.nvim',
-  'neovim/nvim-lspconfig',
-  {'hrsh7th/nvim-cmp'},
-  {'hrsh7th/cmp-nvim-lsp'},
-  {'L3MON4D3/LuaSnip'},
-  'folke/neodev.nvim',
   {'EliasA5/pastem', config = function() require('pastem').setup() end},
   'stefandtw/quickfix-reflector.vim',
   {
