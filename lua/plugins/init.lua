0c8085737be41faebf5ad158d747efd3cdbac1f5
Elias Assaf <elyas51000@gmail.com>
2024-06-10 01:12:10 +0300

    move template to lazy
    Signed-off-by: Elias Assaf <elyas51000@gmail.com>

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -6,17 +6,6 @@ return
   },
   {'EliasA5/pastem', config = function() require('pastem').setup() end},
   'stefandtw/quickfix-reflector.vim',
-  {
-    'nvimdev/template.nvim',
-    cmd = {'Template',},
-    config = function()
-      require('template').setup({
-        temp_dir = "~/templates",
-        author = "Elias Assaf",
-        email = "elyas51000@gmail.com"
-      })
-    end
-  },
   -- disable on nvim 0.10.*
   -- enable on nvim 0.10.*
   -- {
