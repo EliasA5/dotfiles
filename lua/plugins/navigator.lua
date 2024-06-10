c4b42ff6fd8c1671a732271b301cf36966b9b4d4
Elias Assaf <elyas51000@gmail.com>
2024-06-10 09:58:47 +0300

    fix navigator error
    Signed-off-by: Elias Assaf <elyas51000@gmail.com>

--- lua/plugins/navigator.lua
+++ lua/plugins/navigator.lua
@@ -1,6 +1,9 @@
 return {
   'numToStr/Navigator.nvim',
   lazy = false,
+  config = function()
+    require('Navigator').setup()
+  end,
   keys = {
     {'<C-h>', '<CMD>NavigatorLeft<CR>', {'n', 't'}},
     {'<C-l>', '<CMD>NavigatorRight<CR>', {'n', 't'}},
