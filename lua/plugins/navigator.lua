bb1315a3024b48acd52f7439a7cbdc274d24f679
Elias Assaf <elyas51000@gmail.com>
2025-03-06 14:15:01 +0200

    Configure navigator to not switch to tmux pane when we are zoomed in
    Signed-off-by: Elias Assaf <elyas51000@gmail.com>

--- lua/plugins/navigator.lua
+++ lua/plugins/navigator.lua
@@ -1,14 +1,26 @@
 return {
   'numToStr/Navigator.nvim',
   lazy = false,
-  config = function()
-    require('Navigator').setup()
-  end,
   keys = {
     {'<C-h>', '<CMD>NavigatorLeft<CR>', {'n', 't'}},
     {'<C-l>', '<CMD>NavigatorRight<CR>', {'n', 't'}},
     {'<C-j>', '<CMD>NavigatorDown<CR>', 'n'},
     {'<C-k>', '<CMD>NavigatorUp<CR>', 'n'},
+  },
+  opts = {
+    -- Save modified buffer(s) when moving to mux
+    -- nil - Don't save (default)
+    -- 'current' - Only save the current modified buffer
+    -- 'all' - Save all the buffers
+    auto_save = nil,
+
+    -- Disable navigation when the current mux pane is zoomed in
+    disable_on_zoom = true,
+
+    -- Multiplexer to use
+    -- 'auto' - Chooses mux based on priority (default)
+    -- table - Custom mux to use
+    mux = 'auto'
   }
 }
 
