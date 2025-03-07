5cd69045f20996d9de487e4eaf3c453005c0b68e
Elias Assaf <elyas51000@gmail.com>
2025-03-07 18:36:24 +0200

    Timer use notify
    Signed-off-by: Elias Assaf <elyas51000@gmail.com>

--- lua/elias/timer.lua
+++ lua/elias/timer.lua
@@ -7,10 +7,23 @@ local function add_timer(fn, waitfirst)
   vim.defer_fn(timedFn, waitfirst)
 end
 
+-- add the following to .bashrc and launch nvim with nvims
+-- nvims () {
+--     if [[ -z "$@" ]]; then
+--         if [[ -f "./Session.vim" ]]; then
+--             nvim -S Session.vim -c 'lua vim.g.savesession = true'
+--         else
+--             nvim -c 'lua vim.g.savesession = true' .
+--         fi
+--     else
+--         nvim "$@"
+--     fi
+-- }
+
 if vim.g.savesession then
   add_timer(function ()
     vim.api.nvim_command("mks!")
-    print("Saved session!")
+    vim.notify("Saved session!", vim.log.levels.INFO)
     return 900000
   end, 10000)
 end
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -13,5 +13,17 @@ return {
   },
   {'echasnovski/mini.splitjoin', version = false, opts = {}},
   {'echasnovski/mini.cursorword', version = false, opts = {}},
-  {'echasnovski/mini.notify', version = false, opts = {}},
+  {
+    'echasnovski/mini.notify',
+    version = false,
+    config = function ()
+      local notify = require('mini.notify')
+      notify.setup()
+      vim.notify = notify.make_notify({
+        ERROR = { duration = 3000 },
+        WARN = { duration = 3000 },
+        INFO = { duration = 2000 },
+      })
+    end,
+  },
 }
