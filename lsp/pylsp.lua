12cadeb7640ca8a0a3c936f3d12caddb1578ea4a
Elias Assaf <elyas51000@gmail.com>
2025-08-28 20:36:45 +0300

    Remove pylsp root dir config
    Signed-off-by: Elias Assaf <elyas51000@gmail.com>

--- lsp/pylsp.lua
+++ lsp/pylsp.lua
@@ -1,15 +1,4 @@
 return {
-  root_dir = function(fname)
-    local util = require('lspconfig.util')
-    local root_files = {
-      'pyproject.toml',
-      'setup.py',
-      'setup.cfg',
-      'requirements.txt',
-      'Pipfile',
-    }
-    return util.root_pattern(unpack(root_files))(fname) or util.find_git_ancestor(fname)
-  end,
   settings = {
     pylsp = {
       configurationSources = {"flake8"},
