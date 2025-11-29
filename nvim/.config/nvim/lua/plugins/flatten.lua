ce4fbaca21f20bd9e7c91fcbff84b13d8064080f
Elias Assaf <elyas51000@gmail.com>
2025-11-29 15:28:31 +0200

    [nvim] improve flatten to reopen the terminal after blocking
    Signed-off-by: Elias Assaf <elyas51000@gmail.com>

--- nvim/.config/nvim/lua/plugins/flatten.lua
+++ nvim/.config/nvim/lua/plugins/flatten.lua
@@ -12,6 +12,10 @@ return {
         open = "alternate",
       },
       hooks = {
+        should_block = function(argv)
+          -- block if we are diffing between files, like in git commit etc
+          return vim.tbl_contains(argv, "-d")
+        end,
         pre_open = function()
           local term = require("toggleterm.terminal")
           local termid = term.get_focused_id()
@@ -23,6 +27,15 @@ return {
           elseif winnr ~= nil then
             vim.api.nvim_set_current_win(winnr)
           end
+        end,
+        block_end = function()
+          -- After blocking ends (for a git commit, etc), reopen the terminal
+          vim.schedule(function()
+            if saved_terminal then
+              saved_terminal:open()
+              saved_terminal = nil
+            end
+          end)
         end,
       },
     }
