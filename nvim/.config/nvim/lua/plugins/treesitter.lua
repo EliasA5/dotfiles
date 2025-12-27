6d34c25197621d3e3d14dff8fededa10b6bd3d07
Elias Assaf <elyas51000@gmail.com>
2025-12-27 22:05:28 +0200

    [nvim] enable plugin maps
    Signed-off-by: Elias Assaf <elyas51000@gmail.com>

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -132,7 +132,7 @@ return {
     init = function()
       -- Disable entire built-in ftplugin mappings to avoid conflicts.
       -- See https://github.com/neovim/neovim/tree/master/runtime/ftplugin for built-in ftplugins.
-      vim.g.no_plugin_maps = true
+      -- vim.g.no_plugin_maps = true
 
     end,
     dependencies = "nvim-treesitter/nvim-treesitter",
